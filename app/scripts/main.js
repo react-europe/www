5d2151e164950a148424308df2e6ab5a467a1eb4
Katiuska Gamero <katiuska.gc@gmail.com>
2015-03-02 21:34:52 +0100

    accordion: fix scroll on collapsing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,3 +25,13 @@ $(".toggle-modal").click(function(){
     $("#talkModal").modal();
     $("#talkModalBody").html($($(this).data("target")).html());
 })
+$(function () {
+    $('#accordion').on('shown.bs.collapse', function (e) {
+        var offset = $('.panel.panel-default > .panel-collapse.in').offset();
+        if(offset) {
+            $('html,body').animate({
+                scrollTop: $('.panel-title a').offset().top -50
+            }, 500); 
+        }
+    }); 
+});
