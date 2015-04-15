a525fa4ff2e76bcd3fd1dcfce12b2bbc2312f4d2
Katiuska Gamero <katiuska.gc@gmail.com>
2015-04-15 03:04:53 +0200

    add service worker, wip

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,3 +35,13 @@ $(function () {
         }
     }); 
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
