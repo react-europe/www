e96a4da108b44eadd0388a156c409738cf930bc6
Patrick Aljord <patcito@gmail.com>
2016-06-23 00:53:08 +0200

    use new speakers photo

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ gulp build
 rm -rvf ../www-build-2017/*
 cp -R dist/* ../www-build-2017/
 cd ../www-build-2017/; git add --all .; git commit -am"latest"; git push;
-ssh app@api.eventlama.com 'cd www/www-build-2017/;git pull origin master'
+ssh app@api.eventlama.com 'cd www/www-2017-build/;git pull origin master'
